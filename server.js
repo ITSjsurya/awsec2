c3d135408a85b95291cc8546db45ab2225a8ab73
ITSjsurya <surya.j@dhatvibs.com>
2026-03-17 13:03:32 +0530

    Backend

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-
 app.use(cors());
 
 // API routes
@@ -11,14 +10,14 @@ app.get("/api/data", (req, res) => {
   res.json({ data: "Hello from Backend 👋" });
 });
 
-// Serve React build
+// Serve frontend build
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
-      res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// IMPORTANT: bind to 0.0.0.0
+// Start server
 app.listen(5000, "0.0.0.0", () => {
   console.log("Server running on port 5000");
 });
